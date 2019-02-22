72398e2b5f9b29da4a05dd0f044ae34929cfc3cf
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-02-21 17:06:01 -0800

    missed functions, whoops!

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -7,16 +7,17 @@ var Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var newitem = new CartItem(product, quantity);
+  this.items.push(newitem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var saveitem = JSON.stringify(this.item);
+  localStorage.setItem('cart', saveitem);
 };
 
 Cart.prototype.removeItem = function(item) {
-  // TODO: Fill in this instance method to remove one item from the cart.
-  // Note: You will have to decide what kind of parameter to pass in here!
+  this.items.splice(item, 1);
 };
 
 var CartItem = function(product, quantity) {
